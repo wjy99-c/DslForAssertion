4c31dd285eb06f6e772b9bf2f7bb7d55662a26fe
wjy99-c <55765926+wjy99-c@users.noreply.github.com>
2022-03-07 18:26:30 -0800

    hardcoded_channel_pattern_undone

--- hardcoded_channel_pattern.py
+++ hardcoded_channel_pattern.py
@@ -14,15 +14,15 @@ class ChannelsCodePattern:
         self.outside_code_def = self.outside_code_def + "using " + self.channel_name + "= DeviceToHostSideChannel" \
                                                                                        "<DeviceToHostSideChannelID," \
                                                                                        " int, true, 8>;\n "
-        self.outside_code_read = "  for (int i = 0; i < sum_parallel[0]; i++) {\n " \
-                                 "      interested = 1;\n"\
-                                 "      std::cout<<\"start reading....\";\n"\
-                                 "      flag[i] = MyDeviceToHostSideChannel::read();\n" \
-                                 "      std::cout<<flag[i]<<\" find an overflow!\";\n" \
-                                 "      std::cout<<\"read success.\";\n" \
-                                 "      if (flag[i]==-1){break;}\n" \
-                                 "}\n" \
-                                 "  std::cout<<\"finish reading...\";\n"
+        self.outside_code_read = "  for (int i = 0; i < channel_sum[0]; i++) {\n " \
+                                 "      interested = 1;\n" \
+                                 "      std::cout<<\"start reading....\";\n" \
+                                 "      flag[i] = " + self.channel_name + "::read();\n" \
+                                                                          "      std::cout<<flag[i]<<\" find an overflow!\";\n" \
+                                                                          "      std::cout<<\"read success.\";\n" \
+                                                                          "      if (flag[i]==-1){break;}\n" \
+                                                                          "}\n" \
+                                                                          "  std::cout<<\"finish reading...\";\n"
 
     def kernel_channel_ef(self) -> str:
         return self.kernel_code
@@ -34,17 +34,30 @@ class ChannelsCodePattern:
         return self.outside_code_def
 
 
+# TODO UNTESTED
 class OverflowPattern(ChannelsCodePattern):
 
     def __init__(self, variable: str):
         channel_name = "MyDeviceToHostSideChannel_Overflow"
         super(OverflowPattern, self).__init__(channel_name)
-        self.kernel_code = "if (" + variable + "[i]<0){\n bool flag=true;\n MyDeviceToHostSideChannel::write(i," \
-                                               "flag);\n } "
+        self.kernel_code = "if (" + variable + "[i]<0){\n " \
+                                               "   bool flag=true;\n " \
+                                               "   " + self.channel_name + "::write(i,flag);\n " \
+                                                                           "    channel_sum[0] = channel_sum[0] + 1\n" \
+                                                                           "} \n"
 
 
+# TODO UNDONE
 class ArrayOutOfSizePattern(ChannelsCodePattern):
 
     def __init__(self):
         channel_name = "MyDeviceToHostSideChannel_Array"
         super(ArrayOutOfSizePattern, self).__init__(channel_name)
+
+
+# TODO UNDONE
+class HangPattern(ChannelsCodePattern):
+
+    def __init__(self):
+        channel_name = "MyDeviceToHostSideChannel_Hang"
+        super(HangPattern, self).__init__(channel_name)
